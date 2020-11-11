733750c5c473a75f6c02f847e6968abd6e8bc5d6
Turqay Umudzade <turgayau@code.edu.az>
2020-11-11 20:16:47 +0400

    add to file added

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from dotenv import load_dotenv
 load_dotenv()
 import os
+import io
 
 def login(username,password):
     username_input = browser.find_element_by_css_selector("input[name='username']")
@@ -24,25 +25,36 @@ def readChat():
     #browser.execute_script("var chat = document.getElementsByClassName('frMpI  -sxBV');chat.scrollBy(0,250)")
     content = browser.find_elements_by_xpath("//div[@class='   CMoMH    _8_yLp  ']//span")
     for c in content:
-        print(c.text)
+        f.write(c.text)
+        f.write("\n")
 
+def readNChats(n):
+    dms=browser.find_elements_by_xpath("//div[@class='        DPiy6            Igw0E     IwRSH      eGOV_         _4EzTm                                                                                                              ']")
+    for x in range(n):
+       try:
+            dms=browser.find_elements_by_xpath("//div[@class='        DPiy6            Igw0E     IwRSH      eGOV_         _4EzTm                                                                                                              ']")
+            dms[x].click()
+            readChat()
+            f.write("---------------")
+            print(x)
+       except IndexError:
+           print(x," chats have been read")
+           break
 
 browser = webdriver.Chrome()
 browser.implicitly_wait(5)
-
 browser.get('https://www.instagram.com/')
 
 login(os.getenv("instaname"),os.getenv("password"))
+
 goToDirect()
+f = io.open("text.txt", "a", encoding="utf-8")
 browser.implicitly_wait(2)
 
-dms=browser.find_elements_by_xpath("//div[@class='        DPiy6            Igw0E     IwRSH      eGOV_         _4EzTm                                                                                                              ']")
-for x in range(40):
-   try:
-        dms=browser.find_elements_by_xpath("//div[@class='        DPiy6            Igw0E     IwRSH      eGOV_         _4EzTm                                                                                                              ']")
-        dms[x].click()
-        readChat()
-        print("-------")
-   except IndexError:
-       break
+readNChats(40)
+
+
+f.close()
+
+
 
