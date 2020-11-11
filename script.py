f9acc7c8243a4df3bd30cf86137c52f71f1d7ff6
Turqay Umudzade <turgayau@code.edu.az>
2020-11-11 04:00:02 +0400

    login to instagram

---
+++ script.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium import webdriver
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+username= os.getenv("instaname")
+password=os.getenv("password")
+
+browser = webdriver.Chrome()
+browser.implicitly_wait(5)
+
+browser.get('https://www.instagram.com/')
+
+username_input = browser.find_element_by_css_selector("input[name='username']")
+password_input = browser.find_element_by_css_selector("input[name='password']")
+
+username_input.send_keys(username)
+password_input.send_keys(password)
+
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
+
+sleep(5)
+
+browser.close()
