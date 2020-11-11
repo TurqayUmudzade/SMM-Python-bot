f706650399028cf392e95f181e77ba11863b1252
Turqay Umudzade <turgayau@code.edu.az>
2020-11-11 18:08:41 +0400

    get messages from one direct added

--- script.py
+++ script.py
@@ -2,34 +2,58 @@ from time import sleep
 from selenium import webdriver
 from dotenv import load_dotenv
 load_dotenv()
-
 import os
-username= os.getenv("instaname")
-password=os.getenv("password")
+
+def login(username,password):
+    username_input = browser.find_element_by_css_selector("input[name='username']")
+    password_input = browser.find_element_by_css_selector("input[name='password']")
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+
+def goToDirect():
+    browser.find_element_by_xpath("//button[@type='submit']").click()
+
+    browser.find_element_by_xpath("//button[ text()='Save Info']").click()
+    sleep(3)
+    browser.find_element_by_xpath("//button[ text()='Not Now']").click()
+
+    browser.find_element_by_css_selector("nav svg[aria-label='Direct']").click()
+
 
 browser = webdriver.Chrome()
 browser.implicitly_wait(5)
 
 browser.get('https://www.instagram.com/')
 
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
+login(os.getenv("instaname"),os.getenv("password"))
+goToDirect()
+browser.implicitly_wait(2)
 
-username_input.send_keys(username)
-password_input.send_keys(password)
+dms=browser.find_element_by_xpath("//div[@class='        DPiy6            Igw0E     IwRSH      eGOV_         _4EzTm                                                                                                              ']")
+dms.click()
 
-# login_button = browser.find_element_by_xpath("//button[@type='submit']")
-# login_button.click()
-
-browser.find_element_by_xpath("//button[@type='submit']").click()
-
-browser.find_element_by_xpath("//button[ text()='Save Info']").click()
-sleep(3)
-browser.find_element_by_xpath("//button[ text()='Not Now']").click()
-
-browser.find_element_by_css_selector("nav svg[aria-label='Direct']").click()
+content = browser.find_elements_by_xpath("//div[@class='   CMoMH    _8_yLp  ']//span")
+for c in content:
+    print(c.text)
 
 
-sleep(5)
+
+
+
+
+
+
+
+# for dm in dms:
+#     browser.implicitly_wait(1)
+#     dm.click()
+
+# content = browser.find_element_by_xpath("//div[@class='   CMoMH    _8_yLp  ']//span")
+# print(content)
+# for c in content:
+#     print(c.text)
+
+
+# sleep(5)
 
 # browser.close()
